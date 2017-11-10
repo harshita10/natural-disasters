6b87efcbef4dc945f270d3e0aa4e6e7dae406606
Keith <code@keithlee.co.uk>
2017-11-10 22:03:20 +0000

    Extracted and filtered the events

---
+++ natural-disasters/api.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+import requests
+
+
+
+############################################################################
+class Eonet(object):
+
+    # ----------------------------------------------------------------------
+    def __init__(self):
+        self.base = "http://eonet.sci.gsfc.nasa.gov/api/v2.1/"
+
+
+ # ----------------------------------------------------------------------
+    def events(self, days):
+        url = "{}{}".format(self.base, "events")
+        try:
+            r = requests.get(url, params={'days': days})
+        except requests.exceptions.HTTPError as e:
+            msg = "{}. \n{} \nResponse: {}".format(e.message,
+                                                   e.request,
+                                                   e.response)
+            print(msg)
+
+        response = r.json()
+        if isinstance(response, dict):
+            return response['events']
+
--- natural-disasters/natural-disasters.py
+++ natural-disasters/natural-disasters.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+from datetime import date, timedelta, datetime
+
 
 from defaultargs.defaultargs import ArgumentParser, defaultargs
 
 from sqlitesql import Sql
+from api import Eonet
 
 
 # ----------------------------------------------------------------------
@@ -14,6 +17,62 @@ def main():
 
     db = Sql()
     db.create_tables()
+
+    rest = Eonet()
+    events = rest.events(days())
+    filteredEvents = filter_events(events)
+    db.insert_events(filteredEvents)
+
+
+# ----------------------------------------------------------------------
+def filter_events(events):
+    filteredEvents = []
+    firstThisMonth = first_of_month()
+    firstLastMonth = first_of_last_month()
+
+
+    for event in events:
+        if in_category(event['categories']):
+            for geometry in event['geometries']:
+                geodate = datetime.strptime(
+                    geometry['date'], "%Y-%m-%dT%H:%M:%SZ").date()
+
+                if geodate < firstThisMonth and geodate >= firstLastMonth:
+                    filteredEvents.append(event)
+                    continue
+    return filteredEvents
+
+
+# ----------------------------------------------------------------------
+def in_category(eventCategories):
+    categories = ["wildfires", "severe storms", "landslides"]
+    for category in eventCategories:
+        if category['title'].lower() in categories:
+            return True
+    else:
+        return False
+
+
+# ----------------------------------------------------------------------
+def first_of_month():
+    today = date.today()
+    return (date(today.year, today.month, 1))
+
+
+# ----------------------------------------------------------------------
+def first_of_last_month():
+    lastMonth = (date.today() - timedelta(days=28))
+    return date(lastMonth.year, lastMonth.month, 1)
+
+# ----------------------------------------------------------------------
+def days():
+    """
+    returns the number of days between today and the 1st of last month
+    """
+
+    days = (date.today() - first_of_last_month()).days
+    return days
+
 
 # ----------------------------------------------------------------------
 @defaultargs
